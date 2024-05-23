6f8ebb7eb12c46ba4226528176a4c9629f487b44
Chillale Sai Ganesh <125571245+sai1306@users.noreply.github.com>
2024-05-23 19:15:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   return (
     <>
       <div className="container">
-      {!isAuthenticated ? (
+      {isAuthenticated ? (
         <Dashboard/>
       ) : (
         <Login onLogin={handleLogin} />
